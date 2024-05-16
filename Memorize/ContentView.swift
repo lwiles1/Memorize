5f76b3c90ca0ee49a24a4d4207b7c14c03a671bc
lwiles1 <lukeawiles@gmail.com>
2024-05-16 15:24:12 +0100

    First commit Setup project and added new view called CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 15).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 15).strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(.orange)
+            }
+        }
     }
 }
 
