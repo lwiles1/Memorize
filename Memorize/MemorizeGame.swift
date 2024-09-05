c210290de1caf1d48487a340f31011564afea175
lwiles1 <lukeawiles@gmail.com>
2024-09-05 11:44:26 -0500

    Added new more Swift-ey (and no I ain't talking Taylor) way to flip the cards. Also watched lecture on enums and Optionals. I will probably add something to do with that tomorrow.

--- Memorize/MemorizeGame.swift
+++ Memorize/MemorizeGame.swift
@@ -20,19 +20,33 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         }
     }
     
-    func choose(_ card: Card) {
-        
+    mutating func choose(_ card: Card) {
+        let chosenIndex = index(of: card)
+        cards[chosenIndex].isFaceUp.toggle()
+    }
+    
+    func index(of card: Card) -> Int {
+        for index in cards.indices {
+            if cards[index].id == card.id {
+                return index
+            }
+        }
+        return 0
     }
     
     mutating func shuffle() {
         cards.shuffle()
     }
     
-    struct Card: Equatable, Identifiable {
+    struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
         var isFaceUp = true
         var isMatched = false
         let content: CardContent
         
         var id: String
+        
+        var debugDescription: String {
+            "\(id): \(content) \(isFaceUp ? "up" : "down") \(isMatched ? "matched" : "")"
+        }
     }
 }
